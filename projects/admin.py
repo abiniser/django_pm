b20b4ba9876bcaa340e74e0739c739dc42c1ee53
abiniser <abinniser@gamil.com>
2024-11-11 00:50:44 +0300

    Admin panel

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+# this for adding things into the admin page
+admin.site.register(models.category)
+admin.site.register(models.projects)
+admin.site.register(models.task)
+
